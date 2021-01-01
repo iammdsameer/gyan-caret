0c49234bbb3c0d03c852a7f622c4d52abba56e6b
Sameer Khan <itsmdsameerkhan@gmail.com>
2021-01-02 03:22:53 +0545

    bug fixes

---
+++ build/precache-manifest.d3ab2d9ff9413599cf709852ea5caaed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a216fc369f3ae420e58875054aea2879",
+    "url": "/gyan.caretsys.tech/index.html"
+  },
+  {
+    "revision": "4b286f0638c76552806c",
+    "url": "/gyan.caretsys.tech/static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "c353ecba1407b01ae39f",
+    "url": "/gyan.caretsys.tech/static/css/main.d91ef87f.chunk.css"
+  },
+  {
+    "revision": "4b286f0638c76552806c",
+    "url": "/gyan.caretsys.tech/static/js/2.73dcf22d.chunk.js"
+  },
+  {
+    "revision": "610359362e2f9d5fa420eb531608a3c9",
+    "url": "/gyan.caretsys.tech/static/js/2.73dcf22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c353ecba1407b01ae39f",
+    "url": "/gyan.caretsys.tech/static/js/main.e60e7c7b.chunk.js"
+  },
+  {
+    "revision": "aa7e0284f9d76454c79c",
+    "url": "/gyan.caretsys.tech/static/js/runtime-main.29718fe6.js"
+  }
+]);
